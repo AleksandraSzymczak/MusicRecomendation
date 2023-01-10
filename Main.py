58fd6b8e54562a5e910b81a1308ec181c19d964f
Szymczak <100854876+AleksandraSzymczak@users.noreply.github.com>
2023-01-10 16:40:32 +0100

    second button logic

--- Main.py
+++ Main.py
@@ -19,7 +19,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Form):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.pushButton.clicked.connect(self.pushButtonClicked)
-
+        self.pushButton_2.clicked.connect(self.pushButton_2_Clicked)
+        
     def setLabels(self):
 
         self.PopWindow.label_4.setText(self.res[0][30])
@@ -53,7 +54,25 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Form):
         self.setLabels()
         self.PopWindow.show()
         self.PopWindow.exec()
+    def pushButton_2_Clicked(self):
 
+        self.content_table = []
+        self.comboBoxes = [self.comboBox,self.comboBox_2,self.comboBox_3,
+                           self.comboBox_4,self.comboBox_5,self.comboBox_6,self.comboBox_7,
+                           self.comboBox_8]
+        self.checkBoxes = [self.checkBox,self.checkBox_2,self.checkBox_3,self.checkBox_4,self.checkBox_5,
+                           self.checkBox_6,self.checkBox_7,self.checkBox_8]
+        for key in range(0,8):
+            if(self.checkBoxes[key].isChecked()):
+                self.content_table.append(self.comboBoxes[key].currentText())
+                print(self.comboBoxes[key].currentText())
+
+        self.res = al.reasult_Manhattan_distance(data,data_for,self.content_table)
+        self.PopWindow = DialogWindow()
+        self.PopWindow.setModal(True)
+        self.setLabels()
+        self.PopWindow.show()
+        self.PopWindow.exec()
 data,data_for = Data_prep.Data_prep()
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
